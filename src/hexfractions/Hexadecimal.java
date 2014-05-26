1deafd62f26b7333d9e8592d574019971d9f4fff
wangdderek <>
2014-05-25 23:52:10 -0400

    Solution to Hex Fractions (w/ code cleanup)

--- src/hexfractions/Hexadecimal.java
+++ src/hexfractions/Hexadecimal.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hexfractions;
 
 import java.math.BigInteger;
 import java.math.BigDecimal;
 import java.util.*;
 
-/**
- *
- * @author derek
- */
 public class Hexadecimal {
 
     static final int MAX_PRECISE_LENGTH = 13;
--- src/hexfractions/Main.java
+++ src/hexfractions/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hexfractions;
 
 import java.io.File;
@@ -10,14 +5,11 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-/**
- *
- * @author derek
- */
 public class Main {
 
     public static void main(String[] args) throws IOException{
-        Scanner scan = new Scanner(new File(args[0]));
+//        Scanner scan = new Scanner(new File(args[0]));
+        Scanner scan = new Scanner(System.in);
         while(scan.hasNextLine()){
             System.out.println(new Hexadecimal(new BigDecimal(Double.parseDouble(scan.nextLine()))));
         }
