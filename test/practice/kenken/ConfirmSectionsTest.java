f6a6a575fbf5045bfb7202ea968900ba25700283
wangdderek <>
2014-06-10 10:37:47 -0400

    Working KenKen solution (from scratch , runs in 2 min and 22 seconds on sample data), added Test classes and sample input file.

---
+++ test/practice/kenken/ConfirmSectionsTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practice.kenken;
+import java.util.List;
+import static practice.kenken.KenKen.createSections;
+import static practice.kenken.KenKen.confirmSections;
+
+
+/**
+ *
+ * @author derek
+ */
+public class ConfirmSectionsTest {
+    public static void main(String[] args) {
+        List<Section> sections = createSections("5, 1, 4, 3+, 2, 3#, 3, 6, 5+, 5, 8, 9, 4+, 7, 3#");
+        int[] knownGoodArr = new int[]{1,3,2,2,1,3,3,2,1};
+        System.out.println(confirmSections(knownGoodArr, sections));
+    }
+
+}
---
+++ test/practice/kenken/IncrementTest.java
@@ -0,0 +1,16 @@
+package practice.kenken;
+
+import java.util.Arrays;
+import static practice.kenken.KenKen.increment;
+
+public class IncrementTest {
+    public static void main(String[] args) {
+        int[] test = new int[9];
+        int count = 0;
+        while (increment(test, 0, 3)) {
+            System.out.println(Arrays.toString(test));
+            count++;
+        }
+        System.out.println("count = " + count);
+    }
+}
